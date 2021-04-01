e22c60c5cbe16e49558c743cfe282359b1303583
Vinícius Calixto Real <viniciuscalixtoreal@gmail.com>
2021-04-01 02:29:18 -0300

    Seeding Service, populando a base.

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 
 namespace SalesWebMvc.Models
 {
     public class Department
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
